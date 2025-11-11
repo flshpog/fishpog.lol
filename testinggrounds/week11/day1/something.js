6d67959fe4dc790902c5dafb9ade71ca85f64cd5
fishpog <126018624+flshpog@users.noreply.github.com>
2025-11-11 13:49:56 -0500

    click click click

---
+++ testinggrounds/week11/day1/something.js
@@ -0,0 +1,39 @@
+let mitchell ={
+    age: 29,
+    height: "6ft",
+    hobbies: ["gaming", "coding", "hiking"]
+}
+
+console.log(mitchell);
+
+let cohen = {
+    age: 27,
+    height: "5ft 10in",
+    hobbies: ["reading", "traveling", "swimming"]
+}   
+
+console.log(cohen);
+
+let jason = {
+    age: 30,
+    height: "6ft 2in",
+    hobbies: ["photography", "cooking", "cycling"]
+}
+
+console.log(jason);
+
+let emma = {
+    age: 25,
+    height: "5ft 6in",
+    hobbies: ["dancing", "painting", "yoga"]
+}
+
+console.log(emma);
+
+let oliver = {
+    age: 28,
+    height: "5ft 11in",
+    hobbies: ["gaming", "running", "gardening"]
+}
+
+console.log(oliver);
